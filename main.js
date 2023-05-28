166665a12cd2386251939e2175834c171abbaf71
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-05-28 13:32:20 +0530

    1

---
+++ main.js
@@ -0,0 +1,49 @@
+//////////////////// H1 /////////////
+
+const H1 = document.createElement("h1");
+const textnode = document.createTextNode("I am H1 Tag");
+H1.appendChild(textnode);
+
+document.body.appendChild(H1);
+
+/////////////// p /////////////
+const p = document.createElement("p");
+const textnodeP = document.createTextNode("I am Tag P");
+p.appendChild(textnodeP);
+
+document.body.appendChild(p);
+
+//////////////////// li /////////////////
+const li = document.createElement("li");
+const textnodeLi = document.createTextNode("I am li Tag ");
+li.appendChild(textnodeLi);
+
+
+document.body.appendChild(li);
+
+
+//////////////// a ////////////////
+const a = document.createElement("a");
+const textnodeA = document.createTextNode("I am A Tag ");
+a.appendChild(textnodeA);
+
+document.body.appendChild(a);
+
+
+
+/////////////////// BR  ///////////////////
+
+const br = document.createElement("br");
+
+
+document.body.appendChild(br);
+
+
+///////////////// small //////////////////
+
+const small = document.createElement("small");
+const textnodeSmall = document.createTextNode("I am small Tag ");
+small.appendChild(textnodeSmall);
+
+document.body.appendChild(small);
+
